4de0af2727e09f6f47d8eb1db8fcae2abc1322ea
guijiann <guijiann@users.noreply.github.com>
2025-11-28 17:11:49 +0000

    Add sms.rs

---
+++ validator-adaptive/controller-adaptive-resolver/sms.rs
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/kidvote_z/validator-adaptive/controller-adaptive-resolver/sms.rs
+// Generated at: 2025-11-27T12:25:21.400Z
+
+
